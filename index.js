6a293c311d42f00db547cbc14a23726349042f5d
Spencer Lazzar <spencerlazzar@yahoo.com>
2020-10-21 21:32:03 -0700

    Not complete

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 
 const app = require('./lib/server.js');
-require('dotenv').config();
-
+const dotenv = require('dotenv').config();
+const mongoose = require('mongoose');
 const PORT = process.env.PORT;
+
+
+
+mongoose.connect('mongodb://localhost/', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+  .then(() => console.log("DB Connection established"))
+  .catch(err => console.log("No", err))
+
+
 
 app.listen(PORT, () => {
   console.log('Server running on port 3000');
